e4b31be6ec16286812268de165252a04da07aeb9
Daria Stepanova <d.stepanova1993@gmail.com>
2023-05-24 15:47:00 +0200

    new refactoring branch

---
+++ components/nav-button/nav-button.js
@@ -0,0 +1,8 @@
+const navigation = document.querySelector('[data-js="navigation"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+
+
+export function createButton() {
+
+}
