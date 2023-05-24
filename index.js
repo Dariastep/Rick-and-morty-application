f6ad77ed1dfe74c4f8fcd0a4d698b3ebe899864a
Nils Fischer <nils@designatic.de>
2023-05-24 12:51:20 +0200

    add pagination

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+/* const maxPage = 1; */
 const page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character?page=<pageIndex>"
+    );
     if (response.ok) {
       const characters = await response.json();
       cardContainer.innerHTML = "";
       const charactersResults = await characters.results;
+      const maxPage = characters.info.pages;
+      pagination.textContent = `${page} / ${maxPage}`;
+      console.log(characters);
       charactersResults.forEach((characterData) => {
         const characterCard = createCharacterCard(characterData);
         cardContainer.append(characterCard);
